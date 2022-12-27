4a14fd5f500d9b5b817b579f9881be38af0040e9
mohan062002 <2020bcs037@sggs.ac.in>
2022-12-27 21:10:28 +0530

    hiding secrete key

--- dbmsproject/settings.py
+++ dbmsproject/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t#sy$d+_6&y)fu)$4bbi6_ix^j8x%gf*losl56s&!ie0_d&+43'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
